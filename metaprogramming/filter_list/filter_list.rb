99973c51a7c1c8ac57f157ea7484dddc65c0975f
Nick Karpenske <randland@gmail.com>
2024-11-11 15:29:50 -0700

    Basic Filter List

---
+++ metaprogramming/filter_list/filter_list.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+class Object
+  def try(name, *args, &blk)
+    send(name, *args, &blk)
+  rescue NoMethodError
+    nil
+  end
+end
+
+class FilterList
+  def initialize(*args)
+    @_arr = Array(args).flatten(1)
+  end
+
+  def self.delegate(*names, to:)
+    names.each do |name|
+      define_method(name) do |*args, &blk|
+        send(to).send(name, *args, &blk)
+      end
+    end
+  end
+
+  delegate :any?,
+           :empty?,
+           :count,
+           :first,
+           :select,
+           :reject,
+           :inject,
+           :map,
+           to: :_arr
+
+  def to_a() = _arr
+
+  private
+
+  attr_reader :_arr
+
+  def method_missing(name, *args, &blk)
+    case name.to_s
+    when /^is_not_/
+      sub_name = name.to_s.gsub(/^is_not_/, "")
+      if any? { _1.respond_to?(sub_name) }
+        self.class.new(reject { _1.try(sub_name) })
+      elsif any? { _1.respond_to?("#{sub_name}?") }
+        self.class.new(reject { _1.try("#{sub_name}?") })
+      else
+        super
+      end
+    when /^is_/
+      sub_name = name.to_s.gsub(/^is_/, "")
+      if any? { _1.respond_to?(sub_name) }
+        self.class.new(select { _1.try(sub_name) })
+      elsif any? { _1.respond_to?("#{sub_name}?") }
+        self.class.new(select { _1.try("#{sub_name}?") })
+      else
+        super
+      end
+    else super
+    end
+  end
+end
--- spec/metaprogramming/filter_list/filter_list_spec.rb
+++ spec/metaprogramming/filter_list/filter_list_spec.rb
@@ -1,8 +1,64 @@
-puts File.join(__dir__.gsub("/spec", ""), File.basename(__FILE__).gsub("_spec.rb", ""))
+require File.join(__dir__.gsub("/spec", ""), File.basename(__FILE__).gsub("_spec.rb", ""))
 require 'ostruct'
 
 RSpec.describe FilterList do
-  it "works" do
-    expect(FilterList.new).to be
+  describe "#initialize" do
+    it "works if provided no items" do
+      list = described_class.new()
+      expect(list).to_not be_nil
+      expect(list).to be_empty
+    end
+
+    it "works if provided one item" do
+      list = described_class.new(:a)
+      expect(list.count).to eq(1)
+    end
+
+    it "works if provided a list of items, comma separated" do
+      list = described_class.new(:a, :b)
+      expect(list.count).to eq(2)
+    end
+
+    it "works if provided a list of items, as an array" do
+      list = described_class.new([:a, :b])
+      expect(list.count).to eq(2)
+    end
+ end
+
+  describe "#first" do
+    it "returns the first element" do
+      list = described_class.new(:a, :b)
+      expect(list.first).to eq(:a)
+    end
+  end
+
+  describe "#is_XXX" do
+    it "filters the list by XXX" do
+      list = described_class.new(1, 2, 3)
+      filtered = list.is_odd
+      expect(filtered.count).to eq(2)
+      expect(filtered.to_a).to eq([1, 3])
+    end
+
+    it "filters by XXX, even if some things don't respond to it" do
+      list = described_class.new(1, 2, 3, :a)
+      filtered = list.is_odd
+      expect(filtered.count).to eq(2)
+      expect(filtered.to_a).to eq([1, 3])
+    end
+
+    it "filters the list by NOT XXX" do
+      list = described_class.new(1, 2, 3)
+      filtered = list.is_not_odd
+      expect(filtered.count).to eq(1)
+      expect(filtered.to_a).to eq([2])
+    end
+
+    it "filters by NOT XXX, even if some things don't respond to it" do
+      list = described_class.new(1, 2, 3, :a)
+      filtered = list.is_not_odd
+      expect(filtered.count).to eq(2)
+      expect(filtered.to_a).to eq([2, :a])
+    end
   end
 end
